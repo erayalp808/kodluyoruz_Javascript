1157d77f69a41b7c3953cfc1f8c53c33ae983f61
Eray Alp <erayalp.e@gmail.com>
2023-05-02 14:57:44 +0300

    toast-showing functions added

--- Odev2/odev2.js
+++ Odev2/odev2.js
@@ -1,11 +1,20 @@
 function newElement() {
     let toDO = document.querySelector("#task").value;
+    let toast;
     if(toDO.trim().length > 0) {
         let toDoLi = document.createElement("li");
         let toDoList = document.querySelector("#list");
         toDoLi.innerHTML = toDO;
         toDoList.append(toDoLi);
         document.querySelector("#task").value = "";
+        //triggering the toast that will be shown when the item added.
+        let toastSuccess = document.querySelector("#liveToast-success")
+        toast = new bootstrap.Toast(toastSuccess);
+        toast.show();
     } else {
+        //triggering the toast that will be shown when the item couldn't be added.
+        let toastError = document.querySelector("#liveToast-error")
+        toast = new bootstrap.Toast(toastError);
+        toast.show();
     }
 }
