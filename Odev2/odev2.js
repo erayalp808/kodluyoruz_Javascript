21aef42a53d18bbd63fd23937c559e5bb29da2c4
Eray Alp <erayalp.e@gmail.com>
2023-05-02 17:44:11 +0300

    .

--- Odev2/odev2.js
+++ Odev2/odev2.js
@@ -1,6 +1,5 @@
 function newElement() {
     let toDO = document.querySelector("#task").value;
-    let toast;
     if(toDO.trim().length > 0) {
         let toDoLi = document.createElement("li");
         let toDoList = document.querySelector("#list");
@@ -8,13 +7,10 @@ function newElement() {
         toDoList.append(toDoLi);
         document.querySelector("#task").value = "";
         //triggering the toast that will be shown when the item added.
-        let toastSuccess = document.querySelector("#liveToast-success")
-        toast = new bootstrap.Toast(toastSuccess);
-        toast.show();
+        $(".success").toast("show");
     } else {
         //triggering the toast that will be shown when the item couldn't be added.
-        let toastError = document.querySelector("#liveToast-error")
-        toast = new bootstrap.Toast(toastError);
-        toast.show();
+        $(".error").toast("show");
     }
 }
+
